f0eb4b59b5b6215cc12de69b3102e97350f29839
Taechawat Konkaew <luangta_5@hotmail.com>
2021-09-18 14:12:47 +0700

    Handling too many requests error

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-import collections
-
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
+from requests.exceptions import HTTPError
 
 from cryptocurrency import CryptoAPI
 
@@ -9,9 +9,26 @@ app = FastAPI(docs_url=None, redoc_url=None)
 templates = Jinja2Templates(directory="templates")
 
 
+class CustomException(Exception):
+    def __init__(self, error_message: str):
+        self.error_message = error_message
+
+
+@app.exception_handler(CustomException)
+async def exception_handler(request: Request, exc: CustomException):
+    return JSONResponse(
+        status_code=429,
+        content={"status_code": 429, "message": exc.error_message},
+    )
+
+
 @app.get("/")
 async def root(request: Request, timeframe: int = 30):
-    api = CryptoAPI(days=timeframe)
+    try:
+        api = CryptoAPI(days=timeframe)
+    except HTTPError as e:
+        raise CustomException(error_message="Too many requests, please wait a minute")
+
     symbols = ",".join(api.coin_df["symbol"])
     n_rows, n_cols = api.correlation.shape
     data_points = []
